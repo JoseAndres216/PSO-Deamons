a9c7dca92d78984bd0d80dd9f309eaf4bed1d6e2
Jose Andrés Rodríguez Rojas <joseandres216@estudiantec.cr>
2025-05-25 16:26:53 -0600

    Comments

--- highTemperatureNotifier.py
+++ highTemperatureNotifier.py
@@ -5,12 +5,18 @@ import subprocess
 
 HIGHTEMPERATUREFLAGROUTE = os.path.join(os.path.dirname(__file__), "HighTemperatureFlag.txt")
 
+'''
+Function to notify the user about high CPU temperature.
+'''
 def notifyHighTemperature():
     try:
         subprocess.run(["notify-send", "Warning: High temperature ⚠️", "CPU temperature is 80°C or higher."])
     except Exception as e:
         print("Notification error:", e)
 
+'''
+Main function to check the temperature flag and notify if high temperature is detected.
+'''
 def main():
     alreadyNotified = False
 
